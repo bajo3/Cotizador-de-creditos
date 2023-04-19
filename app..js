d2388dc14d0dd85c89a984eb2cc63d6177dfcf1c
bajo3 <you@example.com>
2023-04-19 01:18:11 -0300

    Completo

--- app..js
+++ app..js
@@ -1,54 +1,57 @@
-// Verificar si los campos obligatorios están llenos
-function validarCampos() {
-    const valorAuto = document.getElementById("valor-auto").value.replace(/,/g, '');
-    const anoAuto = document.getElementById("ano-auto").value;
-    const cuotas = document.getElementById("cuotas").value;
+window.addEventListener('load', function() {
+    var loader = document.querySelector('.loader');
+    loader.classList.add('hide');
 
-    if (valorAuto === "" || isNaN(valorAuto) || anoAuto === "" || isNaN(anoAuto)) {
-        document.getElementById("valor-auto").classList.add("required");
-        document.getElementById("ano-auto").classList.add("required");
-        return false;
-    } else {
-        document.getElementById("valor-auto").classList.remove("required");
-        document.getElementById("ano-auto").classList.remove("required");
-        return true;
-    }
-}
-
-// Dar formato de comas a los números mientras se escriben
-function agregarComas(input) {
-    const num = input.value.replace(/\./g, '').replace(/\,/g, '');
-    const numConComas = num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    input.value = numConComas;
-}
-
-document.getElementById("valor-auto").addEventListener("input", function() {
-    agregarComas(this);
+    
+    document.getElementById("cotizar").addEventListener("click", function() {
+        const valorAuto = parseFloat(document.getElementById("valor-auto").value.replace(/,/g, ''));
+        const anoAuto = parseInt(document.getElementById("ano-auto").value);
+    
+        // Verificar si los campos obligatorios están llenos
+        if (isNaN(valorAuto) || isNaN(anoAuto)) {
+            document.getElementById("valor-auto").classList.add("required");
+            document.getElementById("ano-auto").classList.add("required");
+            return;
+        } else {
+            document.getElementById("valor-auto").classList.remove("required");
+            document.getElementById("ano-auto").classList.remove("required");
+        }
+    
+        const edadAuto = new Date().getFullYear() - anoAuto;
+        let tasaInteres;
+    
+        if (edadAuto >= 0 && edadAuto <= 10) {
+            tasaInteres = 0.8;
+        } else if (edadAuto > 10 && edadAuto <= 20) {
+            tasaInteres = 0.88;
+        } else if (edadAuto > 20) {
+            tasaInteres = 0.95;
+        }
+    
+        const cuotas = parseInt(document.getElementById("cuotas").value);
+    
+        const valorCuota = (valorAuto * tasaInteres) / cuotas;
+        const valorCuotaConComas = valorCuota.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    
+        document.getElementById("resultado").innerHTML = `El valor de la cuota es: ${valorCuotaConComas}`;
+    });
+    
+    // Resaltar en rojo los campos que faltan llenar
+    document.getElementById("valor-auto").addEventListener("blur", function() {
+        if (this.value === '') {
+            this.classList.add("required");
+        } else {
+            this.classList.remove("required");
+        }
+    });
+    
+    document.getElementById("ano-auto").addEventListener("blur", function() {
+        if (this.value === '') {
+            this.classList.add("required");
+        } else {
+            this.classList.remove("required");
+        }
+    });
+    
 });
 
-// Calcular valor de la cuota
-document.getElementById("cotizar").addEventListener("click", function() {
-    if (!validarCampos()) {
-        return;
-    }
-
-    const valorAuto = parseFloat(document.getElementById("valor-auto").value.replace(/,/g, ''));
-    const anoAuto = parseInt(document.getElementById("ano-auto").value);
-    const cuotas = parseInt(document.getElementById("cuotas").value);
-
-    const antiguedadAuto = new Date().getFullYear() - anoAuto;
-    let tasaInteres;
-
-    if (antiguedadAuto >= 0 && antiguedadAuto <= 10) {
-        tasaInteres = 0.8;
-    } else if (antiguedadAuto > 10 && antiguedadAuto <= 20) {
-        tasaInteres = 0.88;
-    } else if (antiguedadAuto > 20) {
-        tasaInteres = 0.95;
-    }
-
-    const valorCuota = (valorAuto * tasaInteres) / cuotas;
-    const valorCuotaConComas = valorCuota.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
-
-    document.getElementById("resultado").innerHTML = `El valor de la cuota es: ${valorCuotaConComas}`;
-});
